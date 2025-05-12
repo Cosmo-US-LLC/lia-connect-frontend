4bf864054258db646617d13ee267a6838a50c350
jawad <jawadakhtar@cosmoinc.co>
2025-05-12 15:53:14 +0500

    .env corrections

--- src/Config/endpoint.js
+++ src/Config/endpoint.js
@@ -1,11 +1,2 @@
-export const baseURL = "http://localhost:3001/v1";
-export const baseURLFlask = "http://localhost:5000/";
-// export const baseURL = "https://api.alfrenhr.com/v1/";
-/*
-daio djiioa dwaij dajoroi jdmksfkm iejsoi nvxfnj kkklsadi ewqf zsed ikjl qewer nkfas fnkvxvcmn fjisaiofjjjake is not avavilable but I can get you in contact with him soon enough
-I hope that will begfhdf rget dtdh segrw sfgdgsd xvcbv jmj oiponmvbnm etrh qweff dxvf sfdg wter qewr sewd vxfv fgh kiuk pokl mbjh
-njknnm uilui gyuj drt awe hyuj tdryipj erw iouuhio wrhf srjn, 
-John Marston was raised by Dutch Van Der Linde. Van Der Linde was later on hunted down
-by Marston himself. gjfgjhf yfjyt drttttttt vnm jfh dfhtxde rwrw xdfv sdgf nccvb kkui
-pok jytjft jhvjghj mbn ngvh fch srfe qwde xvfd mbhj cnvb 
-*/
+export const baseURL = process.env.REACT_APP_BACKEND_BASE_URL;
+export const baseURLFlask = process.env.REACT_APP_BASE_URL_FLASK;
